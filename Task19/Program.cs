8968bafbc11e0239dd8843d222fe37ba116d5567
Kirich <kirya65@gmail.com>
2022-12-19 02:28:32 +0300

    Task 19 was accomplished

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+Console.WriteLine("Напишите пятизначное число, чтобы узнать, является ли оно палиндромом: ");
+int N = Convert.ToInt32(Console.ReadLine());
+string ForLenght = Convert.ToString(N);
+int Nlenght = ForLenght.Length;
+int units = N % 10;
+int dozens = N % 100 / 10;
+int thousands = N / 1000 % 10;
+int tenthousands = N / 10000;
+if (Nlenght != 5)   
+    while (Nlenght != 5){
+        Console.WriteLine("Число не пятизначное\nНапишите пятизначное число, чтобы узнать, является ли оно палиндромом: ");
+        N = Convert.ToInt32(Console.ReadLine());
+        ForLenght = Convert.ToString(N);
+        Nlenght = ForLenght.Length;
+        units = N % 10;
+        dozens = N % 100 / 10;
+        thousands = N / 1000 % 10;
+        tenthousands = N / 10000;
+    }
+    if (units == tenthousands && dozens == thousands)
+        Console.WriteLine("Да, это число является палиндромом");
+    else 
+        Console.WriteLine("Нет, это число не является палиндромом");
