3353696194fa5f6e74457d847c955f21bcfb6153
Kirich <kirya65@gmail.com>
2022-12-19 01:19:44 +0300

    Folder DZ3 was restructured and task 23 was accomplished

---
+++ Task23/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Напишите число, чтобы узнать квадраты чисел от 1 до этого числа:");
+int N = Convert.ToInt32(Console.ReadLine());
+int i = 1;
+for (i = 1; i < N; i++)
+    Console.Write($"{i * i * i} ");
