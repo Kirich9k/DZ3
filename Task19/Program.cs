1e93a416b1451665f8c3cf80893e7a00fceaadda
Kirich <kirya65@gmail.com>
2022-12-19 11:34:09 +0300

    Task 19 was upgraded

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -1,23 +1,16 @@
 ﻿Console.Clear();
 Console.WriteLine("Напишите пятизначное число, чтобы узнать, является ли оно палиндромом: ");
 int N = Convert.ToInt32(Console.ReadLine());
+while (N < 10000 || N > 99999){
+        Console.WriteLine("Число не пятизначное\nНапишите пятизначное число, чтобы узнать, является ли оно палиндромом: ");
+        N = Convert.ToInt32(Console.ReadLine());
+}
 string ForLenght = Convert.ToString(N);
 int Nlenght = ForLenght.Length;
 int units = N % 10;
 int dozens = N % 100 / 10;
 int thousands = N / 1000 % 10;
 int tenthousands = N / 10000;
-if (Nlenght != 5)   
-    while (Nlenght != 5){
-        Console.WriteLine("Число не пятизначное\nНапишите пятизначное число, чтобы узнать, является ли оно палиндромом: ");
-        N = Convert.ToInt32(Console.ReadLine());
-        ForLenght = Convert.ToString(N);
-        Nlenght = ForLenght.Length;
-        units = N % 10;
-        dozens = N % 100 / 10;
-        thousands = N / 1000 % 10;
-        tenthousands = N / 10000;
-    }
     if (units == tenthousands && dozens == thousands)
         Console.WriteLine("Да, это число является палиндромом");
     else 
