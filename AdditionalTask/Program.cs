beda2cf9b3c238ffa84672b37aa0d08f26a3a0e3
Kirich9k <118820907+Kirich9k@users.noreply.github.com>
2023-01-23 21:31:33 +0300

    Additional task accomplished

--- AdditionalTask/Program.cs
+++ AdditionalTask/Program.cs
@@ -1,23 +1,29 @@
-﻿Console.Clear();
-Console.WriteLine("Введите число кустов черники (от 3 включительно до 1000 включительно): ");
-int nBush = Convert.ToInt32(Console.ReadLine());
-while (nBush < 3 || nBush > 1000){
-       Console.WriteLine($"Данное число не удовлетворяет условию\nВведите число кустов черники (от 3 включительно до 1000 включительно): ");
-       nBush = Convert.ToInt32(Console.ReadLine());
+Console.Clear();
+Console.WriteLine("Введите число кустов черники: ");
+int nBush = Convert.ToInt32(Console.ReadLine()), resultSum = 0;
+while (nBush < 3 || nBush > 1000)
+{
+    Console.WriteLine($"Данное число не удовлетворяет условию\nВведите число кустов черники (от 3 включительно до 1000 включительно): ");
+    nBush = Convert.ToInt32(Console.ReadLine());
 }
 int[] array = new int[nBush];
-for (int i = 0; i < array.Length + 2; i++){
+for (int i = 0; i < array.Length; i++)
+{
     Console.WriteLine($"Введите {i + 1} элемент массива (элемент не должен быть от 0 до 1000 включительно): ");
     array[i] = Convert.ToInt32(Console.ReadLine());
-    while (array[i] < 0 || array[i] > 1000){
-       Console.WriteLine($"Данное число не удовлетворяет условию\nВведите {i + 1} элемент массива (элемент не должен быть от 0 до 1000 включительно): ");
-       array[i] = Convert.ToInt32(Console.ReadLine());
+    while (array[i] < 0 || array[i] > 1000)
+    {
+        Console.WriteLine($"Данное число не удовлетворяет условию\nВведите {i + 1} элемент массива (элемент не должен быть от 0 до 1000 включительно): ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
     }
-    array[array.Length] = array[0];
-    array[array.Length + 1] = array[1];
-    int sum = array[i - 1] + array[i] + array[i + 1];
-    int summax = 0;
-    if (summax > sum)
-        summax = sum;
-        Console.WriteLine($"{summax}");
 }
+for (int i = 1; i < array.Length - 1; i++)
+{
+    if (resultSum < array[i - 1] + array[i] + array[i + 1])
+        resultSum = array[i - 1] + array[i] + array[i + 1];
+}
+if (resultSum < array[nBush - 1] + array[nBush - 2] + array[0])
+    resultSum = array[nBush - 1] + array[nBush - 2] + array[0];
+if (resultSum < array[0] + array[1] + array[nBush - 1])
+    resultSum = array[0] + array[1] + array[nBush - 1];
+Console.WriteLine(resultSum);
